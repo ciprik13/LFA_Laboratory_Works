8ef106419a861b3cdeaa20297d7161f6a4d55b39
ciprik13 <ciprianmoisenco.it@gmail.com>
2025-02-15 22:41:30 +0200

    Created the FiniteAutomaton Class

--- Lab 1/1_RegularGrammars.py
+++ Lab 1/1_RegularGrammars.py
@@ -25,3 +25,26 @@ class Grammar:
                     string = string.replace(v, random.choice(self.p[v]), 1)
                     break
         return string
+
+class FiniteAutomaton:
+    def __init__(self, states, alphabet, transitions, start_state, final_states):
+        self.states = states
+        self.alphabet = alphabet
+        self.transitions = transitions
+        self.start_state = start_state
+        self.final_states = final_states
+
+    def string_belongs_to_language(self, input_string):
+        current_state = self.start_state
+        for c in input_string:
+            if c not in self.alphabet or current_state not in self.transitions:
+                return False
+            next_state = None
+            for transition in self.transitions[current_state]:
+                if c == transition[0]:
+                    next_state = transition[1] if transition[1] is not None else current_state
+                    break
+            if next_state is None:
+                return False
+            current_state = next_state
+        return current_state in self.f
